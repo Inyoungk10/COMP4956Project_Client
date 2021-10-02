219385e26b82f3dde025e02a82b2c97eb45c3689
cameronwark7 <47673892+cameronwark7@users.noreply.github.com>
2021-10-01 18:03:55 -0700

    added navbar component

---
+++ front-end/client-front-end/src/components/Navbar.js
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return(
+        <div>
+            <a>Home</a>
+            <a>Log in</a>
+            <a>Sign up</a>
+        </div>
+    )
+}
+
+export default Navbar;
