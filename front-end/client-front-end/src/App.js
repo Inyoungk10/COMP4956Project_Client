b16e883ed46b524fd809796a984ca851054c50d8
cameronwark7 <47673892+cameronwark7@users.noreply.github.com>
2021-10-10 22:45:23 -0700

    added unity web gl files

--- front-end/client-front-end/src/App.js
+++ front-end/client-front-end/src/App.js
@@ -1,11 +1,33 @@
 import Navbar from './components/Navbar';
 
+import Unity, { unityContext } from 'react-unity-webgl';
+
+const unityContext = new UnityContext({
+  loaderUrl: "build/myunityapp.loader.js",
+  dataUrl: "build/myunityapp.data",
+  frameworkUrl: "build/myunityapp.framework.js",
+  codeUrl: "build/myunityapp.wasm",
+});
+
+// const unityContext = new UnityContext({
+//   loaderUrl: 'buildUnity/ContainerApp.loader.js',
+//   dataUrl: 'buildUnity/ContainerApp.data',
+//   frameworkUrl: 'buildUnity/ContainerApp.framework.js',
+//   codeUrl: 'buildUnity/ContainerApp.wasm',
+//   webglContextAttributes: {
+//     preserveDrawingBuffer: true,
+//   },
+// });
+
 function App() {
-  return (
-    <div className="App">
-      <Navbar/>
-    </div>
-  );
+  return(
+    <Unity unityContext={unityContext} />
+  )
+  // return (
+  //   <div className="App">
+  //     <Navbar/>
+  //   </div>
+  // );
 }
 
 export default App;
