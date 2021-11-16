299850907728afc69db205c617ce8b26579f0457
inyoungk10 <inyoungk10@gmail.com>
2021-11-16 14:51:20 -0800

    set up mongo backend

--- back-end/app.js
+++ back-end/app.js
@@ -1,14 +1,22 @@
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config({ path: "./config.env" });
 
 const app = express();
 app.use(cors());
 
-// body paser middleware
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+const port = process.env.PORT || 5000;
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`listening on port ${port}...`));
+app.use(express.json());
+app.use(require("./routes/record"));
+// get driver connection
+const dbo = require("./db/conn");
+ 
+app.listen(port, () => {
+  // perform a database connection when server starts
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
+ 
+  });
+  console.log(`Server is running on port: ${port}`);
+});
