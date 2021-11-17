b4f4ae397406dc37336dbbd173633e98dcb5d0ce
cameronwark7 <47673892+cameronwark7@users.noreply.github.com>
2021-11-16 20:11:45 -0800

    created routes

--- back-end/app.js
+++ back-end/app.js
@@ -2,6 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import User from './models/user.js';
 
 const app = express();
 app.use(cors());
@@ -9,6 +12,48 @@ app.use(cors());
 // body paser middleware
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+
+// not sure if they need to be async or not.
+app.post('/signin', async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const existingUser = await User.findOne({ email });
+
+        if(!existingUser) return res.status(404).json({ message: "User doesn't exist." });
+
+        const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
+        if(!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials." });
+
+        const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, 'test', { expiresIn: "1h" });
+
+        res.status(200).json({ result: existingUser, token });
+    } catch (error) {
+        res.status(500).json({ message: "Something went wrong." });
+    }
+});
+
+app.post('/signup', async (req, res) => {
+    const { firstName, lastName, email, password, repeatPassword } = req.body;
+
+    try {
+        const existingUser = await User.findOne({ email });
+        if(existingUser) return res.status(400).json({ message: "User already exists." });
+
+        if(password !== repeatPassword) return res.status(400).json({ message: "Passwords don't match." });
+
+        const hashedPassword = await bcrypt.hash(password, 12);
+
+        const result = await User.create({ email, password: hashedPassword, name: `${firstName} ${lastName}` });
+
+        const tokent = jwt.sign({ email: result.email, id: result._id }, 'test', { expiresIn: "1h" });
+
+        res.status(200).json({ result, token });
+    } catch (error) {
+        res.status(500).json({ message: "Something went wrong." });
+    }
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port}...`));
